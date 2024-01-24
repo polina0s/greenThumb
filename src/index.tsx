8facf81a9037c3eb8a38650270804be22ae33761
polina0s <polina.osintseva000@gmail.com>
2024-01-24 18:48:47 +0400

    removed postcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import './main.scss';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 
 import { MyApp } from './components/app';
 
-createRoot(document.getElementById('root')).render(<MyApp />);
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <MyApp />
+  </QueryClientProvider>,
+);
