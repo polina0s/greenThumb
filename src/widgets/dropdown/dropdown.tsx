017fa3b3f97e9d8c399ab0e362563e62907bfaaf
polina0s <polina.osintseva000@gmail.com>
2024-03-11 16:44:43 +0400

    added dropdown component

--- src/widgets/dropdown/dropdown.tsx
+++ src/widgets/dropdown/dropdown.tsx
@@ -1,22 +1,7 @@
-import Select from 'react-dropdown-select';
-
 import { Box, BoxProps } from '../../components/box';
-
-const list = [
-  {
-    value: 1,
-    label: 'Leanne Graham',
-  },
-  {
-    value: 2,
-    label: 'Ervin Howell',
-  },
-];
 
 export function Dropdown({ title }: BoxProps) {
   return (
-    <Box title={title}>
-      {/* <Select values={list} options={list} /> */}
-    </Box>
+    <Box title={title}>{/* <Select values={list} options={list} /> */}</Box>
   );
 }
