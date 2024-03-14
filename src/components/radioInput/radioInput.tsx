4e0cb0ecf7a714c86f2aaa1b2e6a6c9712adf639
polina0s <polina.osintseva000@gmail.com>
2024-03-14 13:47:44 +0400

    added react-hook-form

--- src/components/radioInput/radioInput.tsx
+++ src/components/radioInput/radioInput.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import * as React from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
 import classes from './radioInput.module.scss';
 
@@ -9,18 +10,14 @@ interface RadioInputProps {
   className?: string;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
-
-export function RadioInput({
-  value,
-  name,
-  className,
-  onChange,
-}: RadioInputProps) {
+export const RadioInput = React.forwardRef(function RadioInput({ className }) {
+  const { control, reset } = useForm<RadioInputProps>({});
   const ContClass = classNames(classes.cont, className);
 
   return (
     <div className={ContClass}>
-      <input
+      <Controller />
+      {/* <input
         className={classes.input}
         type="radio"
         value={value}
@@ -29,7 +26,31 @@ export function RadioInput({
       />
       <label className={classes.name} htmlFor={value}>
         {name}
-      </label>
+      </label> */}
     </div>
   );
-}
+});
+
+// export function RadioInput({
+//   value,
+//   name,
+//   className,
+//   onChange,
+// }: RadioInputProps) {
+// const ContClass = classNames(classes.cont, className);
+
+// return (
+//   <div className={ContClass}>
+//     <input
+//       className={classes.input}
+//       type="radio"
+//       value={value}
+//       id={value}
+//       onChange={onChange}
+//     />
+//     <label className={classes.name} htmlFor={value}>
+//       {name}
+//     </label>
+//   </div>
+// );
+// }
