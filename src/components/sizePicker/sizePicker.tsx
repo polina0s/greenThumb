d5fe3497309aeef27555f19f48aaa9ac4c9b1b0c
polina0s <polina.osintseva000@gmail.com>
2024-05-15 17:23:45 +0400

    fixed size picker component

--- src/components/sizePicker/sizePicker.tsx
+++ src/components/sizePicker/sizePicker.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+
 import { Text } from '../text';
 import classes from './sizePicker.module.scss';
 
 export function SizePicker() {
+  const [checked, setChecked] = useState('');
+
   return (
     <div>
       <Text
@@ -12,9 +16,39 @@ export function SizePicker() {
         Size
       </Text>
       <div className={classes.optionsCont}>
-        <button className={classes.optionButton}>S</button>
-        <button className={classes.optionButton}>M</button>
-        <button className={classes.optionButton}>L</button>
+        <label>
+          <input
+            type="radio"
+            value="S"
+            onChange={(e) => setChecked(e.target.value)}
+            checked={checked === 'S'}
+          />
+          <div className={classes.optionBox}>
+            <span className={classes.optionSize}>S</span>
+          </div>
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="M"
+            onChange={(e) => setChecked(e.target.value)}
+            checked={checked === 'M'}
+          />
+          <div className={classes.optionBox}>
+            <span className={classes.optionSize}>M</span>
+          </div>
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="L"
+            onChange={(e) => setChecked(e.target.value)}
+            checked={checked === 'L'}
+          />
+          <div className={classes.optionBox}>
+            <span className={classes.optionSize}>L</span>
+          </div>
+        </label>
       </div>
     </div>
   );
