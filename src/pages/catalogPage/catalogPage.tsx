2cb1adbc055da14feb59d54ce805d35b0bb290b1
polina0s <polina.osintseva000@gmail.com>
2024-08-27 19:38:41 +0400

    fixed catalogPage

--- src/pages/catalogPage/catalogPage.tsx
+++ src/pages/catalogPage/catalogPage.tsx
@@ -1,21 +1,55 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import { CatalogCard } from '../../components/catalogCard';
 import { Footer } from '../../components/footer';
 import { Header } from '../../components/header';
 import { SectionBanner } from '../../components/sectionBanner';
+import { getShopItems } from '../../store/shopItems/shopItems.actions';
+import { allShopItemsSelector } from '../../store/shopItems/shopItems.selectors';
+import { useAppDispatch } from '../../store/store';
 import { SelectSort } from '../../widgets/select';
 import { CatalogFilters } from '../../widgets/сatalogFilters/сatalogFilters';
 import classes from './catalogPage.module.scss';
 
 export function CatalogPage() {
+  const dispatch = useAppDispatch();
+  const shopItems = useSelector(allShopItemsSelector);
+
+  useEffect(() => {
+    dispatch(getShopItems({ limit: 9 }));
+  }, [dispatch]);
+
   return (
     <div className={classes.wrapper}>
       <Header promoTitle="FREE SHIPPING ON ALL FULL SUN PLANTS! FEB. 25–28." />
-      <SectionBanner
-        title="Shop"
-        description="Find the perfect plant for your space"
-      />
-      <CatalogFilters />
-      <SelectSort />
-      <Footer />
+      <div className={classes.banner}>
+        <SectionBanner
+          title="Shop"
+          description="Find the perfect plant for your space"
+        />
+      </div>
+      <div className={classes.selectSort}>
+        <SelectSort />
+      </div>
+      <div className={classes.catalog}>
+        <div className={classes.filters}>
+          <CatalogFilters />
+        </div>
+        <div className={classes.cards}>
+          {shopItems.map((item) => {
+            return (
+              <CatalogCard
+                title={item.name}
+                price={item.price}
+                imgSrc={item.images[1].src}
+                key={item.id}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <Footer variant="green" />
     </div>
   );
 }
